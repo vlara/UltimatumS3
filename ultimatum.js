e73856bd068d127c2a4f772d6b8dd904006f2c66
Daniel <howtomen@gmail.com>
2012-08-03 11:28:00 -0700

    Rom Toolbox file

---
+++ ultimatum.js
@@ -0,0 +1,14 @@
+{
+    "version": "1", /*Never edit this line, this stays version 1*/
+    "homepage": "http://www.ultimatum.us", /*This is the link to the OP. If you have a website, this is where you would link the website*/
+    "donate": "http://forum.xda-developers.com/donatetome.php?u=3471910", /*Donation Link*/
+    "roms": [
+        {
+            "name": "Ultimatum S3 1.3: Simply Amazing! Fast Light Customizable", /*This is the name that shows up inside the actual Rom category. This is where you want to mention your update number, update name, specific date, that kind of thing.*/
+            "product": "Team Ultimatum", /*This is where the team name or dev name would be placed */
+            "summary": "1.3 Fixed all MMS issues (added all CSC back) Added MMS options in Aroma Redid all Status Bar mods Removed Carrier logo in status Bar Changed Status Bar (to add brightness Slider) Removed ongoing wifi notification updated Gmail  updated Youtube updated Chrome updated Google Music Updated Play Store", /*Originally, this line had a summary of the Rom. Since this summary is now shown on the Overall Rom List under Ultimatum, this area can now be used for changelog info, starting in something like (7/31): Updated base...*/
+            "device": "d2att", /*Device name, do not edit unless you start making this rom for other devices, in which case...contact me :)*/
+            "url": "http://www.holylivingfuck.com/Howtomen/1.3.zip" /*Direct link to rom download. if there is any sort of wait or button press (ie Goo.im), the download will error out*/
+        }
+    ]
+}
